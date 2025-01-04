c9ef84c148a0cf2b43c3ca18085b757caeaa6f28
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-04 18:44:31 +0500

    Add files via upload

---
+++ fill 2d vector.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> a;
+    for(int i = 0; i < n; i++){
+        vector<int> row;
+        for(int j = 0; j < n; j++){
+            if(i == 0 and j == 0 or
+               i == 0 and j == n - 1 or
+               i == n - 1 and j == 0 or
+               i == n - 1 and j == n - 1) row.push_back(1);
+            else row.push_back(0);
+        }
+        a.push_back(row);
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
+}
+

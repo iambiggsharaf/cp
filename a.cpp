669e15375efe9cf87ee53a6dccb044b9f8f10520
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-05 11:13:00 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,14 +1,25 @@
 #include <bits/stdc++.h>
- using namespace std;
- int main (){
+using namespace std;
+int main() {
     int n;
     cin >> n;
-    string s;
-    while(n != 0){
-        if(n % 2 == 0) s += '0';
-        if(n % 2 == 1) s += '1';
-        n /= 2;
+    vector<vector<int>> a;
+    for(int i = 0; i < n; i++){
+        vector<int> row;
+        for(int j = 0; j < n; j++){
+            if(i == 0 or
+               j == n - 1 or
+               j == 0 or
+               i == n - 1) row.push_back(1);
+            else row.push_back(0);
+        }
+        a.push_back(row);
     }
-    reverse(s.begin(), s.end());
-    cout << s;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << a[i][j];
+        }
+        cout << endl;
+    }
 }
+

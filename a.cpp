9a5c1cc2f0194fd432ed45259c88f10b7a5663e7
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-08 18:33:38 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -4,22 +4,19 @@ int main (){
     int n;
     cin >>n;
     vector < vector <int> > arr;
-
     for(int i = 0 ; i < n; i++){
         vector <int> row;
         for(int j = 0; j < n; j++){
-            if(i % 2 == 0) row.push_back(1);
+            if(j % 2 != 0) row.push_back(1);
             else row.push_back(0);
         }
         arr.push_back(row);
     }
-
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             cout << arr[i][j];
         }
         cout << endl;
     }
-
     return 0;
 }

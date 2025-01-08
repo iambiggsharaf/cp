8c6bdc70173022741fdb62a99abbc961dcd211b3
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-08 19:33:42 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -7,15 +7,18 @@ int main (){
     for(int i = 0; i < n; i++){
         vector <int> row;
         for(int j = 0; j < m; j++){
-            if(i >= y and i <= y1 and j >= x and j <= x1) row.push_back(1);
+            if(i == y and j >= x and j <= x1 or
+                i == y1 and j >= x and j <= x1 or
+                j == x and i >= y and i <= y1 or
+                j == x1 and i >= y and i <= y1) row.push_back(1);
+            else if(i > y and i < y1 and j > x and j < x1) row.push_back(2);
             else row.push_back(0);
         }
         arr.push_back(row);
     }
-
     for(auto row: arr){
         for(auto ele: row){
-            cout << ele << " ";
+            cout << ele;
         }
         cout << "\n";
     }

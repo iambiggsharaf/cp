e8173716bea2ea16b74e96a1e86e7c1c076e439e
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-08 18:29:48 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    int n, x;
-    cin >> n;
-    vector<vector<int>> a;
-    for(int i = 0; i < n; i++){
-        vector<int> row;
+int main (){
+    int n;
+    cin >>n;
+    vector < vector <int> > arr;
+
+    for(int i = 0 ; i < n; i++){
+        vector <int> row;
         for(int j = 0; j < n; j++){
-            if(j >= n / 2) row.push_back(1);
+            if(i == j or i + j == n - 1) row.push_back(1);
             else row.push_back(0);
         }
-        a.push_back(row);
+        arr.push_back(row);
     }
-    for(auto i: a){
-        for(auto j: i){
-            cout << j;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << arr[i][j];
         }
-        cout << "\n";
+        cout << endl;
     }
- }
+
+    return 0;
+}

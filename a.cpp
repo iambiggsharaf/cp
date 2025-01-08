a0514cbc0662bc3e9448b4f19bcc6873a179d376
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-08 21:00:18 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main (){
-    int n = 10, m = 10, cnt = 0, temp;
+    int n = 10, m = 10, cnt1 = 0, cnt2 = 0, temp;
     vector < vector <int> > arr;
     for(int i = 0; i < n; i++){
         vector <int> row;
         for(int j = 0; j < m; j++){
             cin >> temp;
+            if(temp == 1) cnt1++;
             row.push_back(temp);
         }
         arr.push_back(row);
     }
     for(int i = 0; i < n - 1; i++){
         for(int j = 0; j < m; j++){
-            if(arr[i][j] == 1 and arr[i + 1][j] == 1) cnt++;
+            if(arr[i][j] == 1 and arr[i + 1][j] == 1) cnt2++;
         }
     }
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m - 1; j++){
-            if(arr[i][j] == 1 and arr[i][j + 1] == 1) cnt++;
+            if(arr[i][j] == 1 and arr[i][j + 1] == 1) cnt2++;
         }
     }
-    cout << cnt;
+    cout << cnt1 - (2 * cnt2) << " " << cnt2;
 }

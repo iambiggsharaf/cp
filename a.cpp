ff6cfec342b4d26d51526ddf3174a15c08d871b5
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-14 13:22:28 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main (){
-    int n, a = 0, b = 0;
-    while(cin >> n){
-        if(n == 0) break;
-        if(n % 6 == 0 and n % 10 == 4) a+=n;
-    }
-    cout << a;
+    string s;
+    cin >> s;
+    cout << s + '0';
 }

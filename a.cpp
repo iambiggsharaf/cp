1837174318920e745df36efd7685ee66cd5345fc
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-15 19:37:14 +0500

    Add files via upload
    Sorting two binary numbers

--- a.cpp
+++ a.cpp
@@ -1,9 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main (){
-    string s;
-    cin >> s;
-    s = bitset<32>(stoll(s, nullptr, 2) - 1).to_string();
-    try {cout << s.substr(s.find('1'));}
-    catch(exception&e) {cout << 0;}
+int main() {
+    string a, b;
+    cin >> a >> b;
+    cout << (stoll(a, nullptr, 2) >= stoll(b, nullptr, 2) ? a+"\n"+b : b+"\n"+a);
 }

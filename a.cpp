4c178171a0ddd3dd378db107084e98320b219e8d
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-15 18:57:15 +0500

    Add files via upload
    Convert from 8 to binary

--- a.cpp
+++ a.cpp
@@ -3,5 +3,5 @@ using namespace std;
 int main (){
     string s;
     cin >> s;
-    for(auto i: s) cout << bitset<4>(stoi(string(1, i), nullptr, 16));
+    for(auto i: s) cout << bitset<3>(stoll(string(1, i), nullptr, 8)).to_string();
 }

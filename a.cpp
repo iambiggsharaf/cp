779b08b271e641f99d985cc04bfa5e192f14a84d
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-15 19:23:41 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main (){
     string s;
     cin >> s;
-    for(int i = 0; i < s.size(); i+=4){
-        cout << uppercase << hex << stoll(s.substr(i, 4), nullptr, 2);
-    }
+    s = bitset<32>(stoll(s, nullptr, 2) - 1).to_string();
+    try {cout << s.substr(s.find('1'));}
+    catch(exception&e) {cout << 0;}
 }

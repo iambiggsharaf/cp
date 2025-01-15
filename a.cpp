e58339da1c1efbb4df4ddc7096be2c349faa2a5f
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-15 19:57:25 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    string a, b;
-    cin >> a >> b;
-    cout << (stoll(a, nullptr, 2) >= stoll(b, nullptr, 2) ? a+"\n"+b : b+"\n"+a);
+    int n;
+    cin >> n;
+    cout << ((n - 2) + 7) % 7 + 1;
 }

154abe61cb8cf5dced1f1e42e637db8b0ead29fc
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-16 15:16:13 +0500

    Add files via upload
    Codeforces

--- a.cpp
+++ a.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int n;
-    cin >> n;
-    vector<int>arr(n);
-    for(int i = 0; i < n; i++) cin >> arr[i];
-    sort(arr.begin(), arr.end());
-    for(auto i: arr) cout << i << " ";
+    int ans, n, cnt = 0;
+    cin >> ans >> n;
+    if(n > 0){
+        vector<int> arr(n);
+        for(int i = 0; i < n; i++) cin >> arr[i];
+        sort(arr.begin(), arr.end());
+        for(int i = 0; i < n; i++){
+                if(arr[i] >= ans){
+                    ans = arr[i];
+                    cnt = 1;
+                    break;
+                }
+        }
+        for(int i = 0; i < n; i++){
+            if(arr[i] - ans >= 3){
+                cnt++;
+                ans = arr[i];
+            }
+        }
+        cout << cnt;
+    }
+    else cout << 0;
 }

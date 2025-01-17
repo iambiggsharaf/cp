6f06b4a3cf3011c2a6b9bbb26726fe994690f201
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-17 09:34:31 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,26 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int ans, n, cnt = 0;
-    cin >> ans >> n;
-    if(n > 0){
-        vector<int> arr(n);
-        for(int i = 0; i < n; i++) cin >> arr[i];
-        sort(arr.begin(), arr.end());
-        for(int i = 0; i < n; i++){
-                if(arr[i] >= ans){
-                    ans = arr[i];
-                    cnt = 1;
-                    break;
-                }
-        }
-        for(int i = 0; i < n; i++){
-            if(arr[i] - ans >= 3){
-                cnt++;
-                ans = arr[i];
-            }
-        }
-        cout << cnt;
-    }
-    else cout << 0;
+    int a, b;
+    cin >> a >> b;
+    cout << (a <= -1 ? "Inside" : "Outside");
 }

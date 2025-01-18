288c7429ac68130d514752cd9f748eb1cf4a3e99
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-18 14:38:34 +0500

    Add files via upload
    Stepik Geometry

--- a.cpp
+++ a.cpp
@@ -3,5 +3,5 @@ using namespace std;
 int main() {
     int x, y;
     cin >> x >> y;
-    cout << ((y >= (x - 5) and (y >= (-x-2))) ? "Inside" : "Outside");
+    cout << ( (x <= 0 and y >= 0 and y <= (x + 7)) or (x >= 0 and y <= 0 and y >= (x + 7))  ? "Inside" : "Outside");
 }

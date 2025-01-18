4fecc2be258d886c231bdbd5d8d280f7aea5af03
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-18 13:16:29 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int a, b;
-    cin >> a >> b;
-    cout << (a <= -1 ? "Inside" : "Outside");
+    int x, y;
+    cin >> x >> y;
+    cout << ((x <= -5) or (x >= -2 and x <= 3 and y >= 3) or (x >= 6) ? "Inside" : "Outside");
 }

a8d001c3ac0795cbfc862828220bed10cfbb2a45
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-22 20:02:16 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -24,12 +24,37 @@ int main() {
     // Считаем пересечение двух множеств
     int count = 0;
     for (int num : set1) {
+        if (set2.count(num)) {#include <iostream>
+#include <set>
+
+int main() {
+    int N, M;
+    std::cin >> N; // Количество элементов первого списка
+
+    std::set<int> set1; // Множество для первого списка
+    for (int i = 0; i < N; ++i) {
+        int num;
+        std::cin >> num;
+        set1.insert(num);
+    }
+
+    std::cin >> M; // Количество элементов второго списка
+
+    std::set<int> set2; // Множество для второго списка
+    for (int i = 0; i < M; ++i) {
+        int num;
+        std::cin >> num;
+        set2.insert(num);
+    }
+
+    int count = 0;
+    for (int num : set1) {
         if (set2.count(num)) {
-            ++count;
+            std::cout << num << " ";
         }
     }
 
-    std::cout << count << std::endl;
+    //std::cout << count << std::endl;
 
     return 0;
 }

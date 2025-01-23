51176f1c16dfafee5cecbb616777a02a8faa3b1f
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-23 10:30:23 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,60 +1,15 @@
-#include <iostream>
-#include <set>
-
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    int N, M;
-    std::cin >> N; // Количество элементов первого списка
-
-    std::set<int> set1; // Множество для первого списка
-    for (int i = 0; i < N; ++i) {
-        int num;
-        std::cin >> num;
-        set1.insert(num);
+    int n;
+    string a, b;
+    cin >> n;
+    map <string, string> dict;
+    while(n--){
+        cin >> a >> b;
+        dict[a] = b;
+        dict[b] = a;
     }
-
-    std::cin >> M; // Количество элементов второго списка
-
-    std::set<int> set2; // Множество для второго списка
-    for (int i = 0; i < M; ++i) {
-        int num;
-        std::cin >> num;
-        set2.insert(num);
-    }
-
-    // Считаем пересечение двух множеств
-    int count = 0;
-    for (int num : set1) {
-        if (set2.count(num)) {#include <iostream>
-#include <set>
-
-int main() {
-    int N, M;
-    std::cin >> N; // Количество элементов первого списка
-
-    std::set<int> set1; // Множество для первого списка
-    for (int i = 0; i < N; ++i) {
-        int num;
-        std::cin >> num;
-        set1.insert(num);
-    }
-
-    std::cin >> M; // Количество элементов второго списка
-
-    std::set<int> set2; // Множество для второго списка
-    for (int i = 0; i < M; ++i) {
-        int num;
-        std::cin >> num;
-        set2.insert(num);
-    }
-
-    int count = 0;
-    for (int num : set1) {
-        if (set2.count(num)) {
-            std::cout << num << " ";
-        }
-    }
-
-    //std::cout << count << std::endl;
-
-    return 0;
+    cin >> a;
+    cout << dict[a];
 }

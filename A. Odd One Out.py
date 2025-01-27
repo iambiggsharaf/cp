0a1be26fe3e88e601909a92c7e3f0565f482c69d
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-27 18:19:05 +0500

    Add files via upload
    Codeforces

---
+++ A. Odd One Out.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(n):
+    a, b, c = map(int, input().split())
+    if a != b and a != c:
+        print(a)
+    elif b != a and b != c:
+        print(b)
+    else:
+        print(c)

08b39e6c9b1d643885a11829eb18385ec49e7bf7
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-01-28 10:58:00 +0500

    Add files via upload
    Leetcode

---
+++ 125. Valid Palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        ans = ""
+        for i in s:
+            if i.isalnum(): ans += i
+        return ans == ans[::-1]
+        

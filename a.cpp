5840fba37cd17648821b53b5fadd93590365c10a
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-02-02 19:33:20 +0500

    Add files via upload
    Stepik Geometry

--- a.cpp
+++ a.cpp
@@ -1,15 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    int n;
-    string a, b;
-    cin >> n;
-    map <string, string> dict;
-    while(n--){
-        cin >> a >> b;
-        dict[a] = b;
-        dict[b] = a;
+int main(){
+    double x, y;
+    cin >> x >> y;
+    bool big_circle = (pow(x,2) + pow(y,2) <= 25);
+    bool small_a = (pow(x+1.5, 2) + pow(y-2, 2) < 1);
+    bool small_b = (pow(x-1.5, 2) + pow(y-2, 2) < 1);
+    bool semi = ((pow(x, 2) + pow(y, 2) < 9) and y < 0);
+    bool teeth = (x >= -1 and x <= 1 and y >= -3 and y <= -2);
+
+    if(big_circle and !small_a and !small_b and (!semi or teeth)){
+        cout << "Inside";
     }
-    cin >> a;
-    cout << dict[a];
+    else{
+        cout << "Outside";
+    }
+
 }

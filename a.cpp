d1a2ead9128709bf4e297337c6458ae9a811b0d0
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-02-02 21:34:01 +0500

    Add files via upload
    Stepik Geometry

--- a.cpp
+++ a.cpp
@@ -3,17 +3,12 @@ using namespace std;
 int main(){
     double x, y;
     cin >> x >> y;
-    bool big_circle = (pow(x,2) + pow(y,2) <= 25);
-    bool small_a = (pow(x+1.5, 2) + pow(y-2, 2) < 1);
-    bool small_b = (pow(x-1.5, 2) + pow(y-2, 2) < 1);
-    bool semi = ((pow(x, 2) + pow(y, 2) < 9) and y < 0);
-    bool teeth = (x >= -1 and x <= 1 and y >= -3 and y <= -2);
+    bool a = (pow(x-5, 2) + pow(y, 2) <= 4 and y <= 0);
 
-    if(big_circle and !small_a and !small_b and (!semi or teeth)){
-        cout << "Inside";
-    }
-    else{
-        cout << "Outside";
-    }
+    bool b = (pow(x, 2) + pow(y-3, 2) < 1);
 
+    bool c = (y <= 5);
+    bool d = (y >= -8*x/3-3);
+    bool e = (y >= 8*x/3-3);
+    cout << ( (a) or (c and d and e and !b) ? "Inside" : "Outside");
 }

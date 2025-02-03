4b58c88f8ea47deb94126633f451607d2deecee3
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-02-03 13:37:14 +0500

    Add files via upload
    Stepik

--- a.cpp
+++ a.cpp
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-bool comparison (pair <int, int> a, pair <int, int> b) {
-
-         return (a.second == b.second) ? a.first < b.first : a.second > b.second;
-
+bool comparison (pair <string, int> a, pair <string, int> b) {
+         return a.second < b.second;
 }
 int main(){
     long long int n, a, b ;
     cin >> n;
-    vector<pair<int, int>> arr;
+    vector<pair<string, int>> arr;
     for(int i = 0; i < n; i++){
         cin >> a >> b;
-        arr.push_back({a, b});
+        string ans = to_string(a) + " " + to_string(b);
+        double c = sqrt(pow(a, 2)+pow(b,2));
+        arr.push_back({ans,c});
     }
     sort(arr.begin(), arr.end(), comparison);
     for(int i = 0; i < n; i++){
-        cout << arr[i].first << " " << arr[i].second<< "\n";
+       cout << arr[i].first << "\n";
+//            cout << arr[i] << " " ;
     }
-
 }

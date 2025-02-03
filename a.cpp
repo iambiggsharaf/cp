8071e460df6f9c46aaaba3081e794cd6d3e4885a
iambiggsharaf <53560643+iambiggsharaf@users.noreply.github.com>
2025-02-03 13:49:21 +0500

    Add files via upload
    Stable Sort - Stepik

--- a.cpp
+++ a.cpp
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool comparison (pair <string, int> a, pair <string, int> b) {
-         return a.second < b.second;
+bool comparison (pair <string, double> a, pair <string, double> b) {
+         return a.second > b.second;
 }
 int main(){
-    long long int n, a, b ;
+    double n, x, y, z;
+    string a, b;
     cin >> n;
-    vector<pair<string, int>> arr;
+    vector <pair<string,double>>arr;
     for(int i = 0; i < n; i++){
-        cin >> a >> b;
-        string ans = to_string(a) + " " + to_string(b);
-        double c = sqrt(pow(a, 2)+pow(b,2));
-        arr.push_back({ans,c});
+        cin >> a >> b >> x >> y >> z;
+        a += " "+b;
+        x = (x+y+z)/3;
+        arr.push_back({a, x});
     }
     sort(arr.begin(), arr.end(), comparison);
-    for(int i = 0; i < n; i++){
-       cout << arr[i].first << "\n";
-//            cout << arr[i] << " " ;
-    }
+    for(auto i:arr) cout << i.first << "\n";
 }
